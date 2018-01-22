da8d87d29bfb0892b2f8db0b02e4d41c6a1c8e23
shilijie <605950892@qq.com>
2018-01-22 17:35:23 +0800

    个人设置

--- 无影灯下 2 2/WUYINGDENGXIA/WUYINGDENGXIA/Class/MyVC/MyDetailViewController.m
+++ 无影灯下 2 2/WUYINGDENGXIA/WUYINGDENGXIA/Class/MyVC/MyDetailViewController.m
@@ -11,9 +11,25 @@
 #import "BRTextField.h"
 #import "NSDate+BRAdd.h"
 
-@interface MyDetailViewController ()<UITableViewDelegate,UITableViewDataSource>
+@interface MyDetailViewController ()<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableview;
+
+@property (nonatomic, strong) BRTextField *nameTF;//昵称
+@property (nonatomic, strong) BRTextField *cityTF;//城市
+@property (nonatomic, strong) BRTextField *yiyuanTF;//医院
+@property (nonatomic, strong) BRTextField *keshiTF;//科室
+@property (nonatomic, strong) BRTextField *zhichengTF;//职称
+@property (nonatomic, strong) BRTextField *zhiwuTF;//职务
+@property (nonatomic, strong) BRTextField *danweiTF;//单位
+@property (nonatomic, strong) BRTextField *zhiweiTF;//职位
+@property (nonatomic, strong) BRTextField *xuexiaoTF;//学校
+@property (nonatomic, strong) BRTextField *zhuanyeTF;//专业
+@property (nonatomic, strong) BRTextField *xueliTF;//学历
+@property (nonatomic, strong) BRTextField *ruxuenianfenTF;//入学年份
+
+@property (nonatomic, strong) NSArray *titleArr;
+
 
 @end
 
@@ -40,6 +56,15 @@
 }
 
 #pragma mark - tableviewDelegate -
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    if (section == 0) {
+        return 0;
+    }else{
+        return 25;
+    }
+    
+}
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 4;
 }
@@ -89,14 +114,149 @@
         cell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:pageID];
         
     }
-    cell.textLabel.text = self.tableViewAyy[indexPath.row];
     
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    switch (indexPath.section) {
+        case 0:
+        {
+            cell.accessoryType = UITableViewCellAccessoryNone;
+            [self setupNameTF:cell];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        }
+            break;
     
+            
+    
+
+    }
     return cell;
-    
-    
 }
+
+- (BRTextField *)getTextField:(UITableViewCell *)cell {
+    BRTextField *textField = [[BRTextField alloc]initWithFrame:CGRectMake(SCREEN_WIDTH - 230, 0, 200, 50)];
+    textField.backgroundColor = [UIColor clearColor];
+    textField.font = [UIFont systemFontOfSize:16.0f];
+    textField.textAlignment = NSTextAlignmentRight;
+    textField.textColor = RGB_HEX(0x666666, 1.0);
+    textField.delegate = self;
+    [cell.contentView addSubview:textField];
+    return textField;
+}
+
+#pragma mark - 姓名 textField
+- (void)setupNameTF:(UITableViewCell *)cell {
+    if (!_nameTF) {
+        _nameTF = [self getTextField:cell];
+        _nameTF.placeholder = @"请输入";
+        _nameTF.returnKeyType = UIReturnKeyDone;
+        _nameTF.tag = 0;
+    }
+}
+- (void)setupcityTF:(UITableViewCell *)cell {
+    if (!_cityTF) {
+        _cityTF = [self getTextField:cell];
+        _cityTF.placeholder = @"请输入";
+        _cityTF.returnKeyType = UIReturnKeyDone;
+        _cityTF.tag = 0;
+    }
+}
+- (void)setupyiyuanTF:(UITableViewCell *)cell {
+    if (!_yiyuanTF) {
+        _yiyuanTF = [self getTextField:cell];
+        _yiyuanTF.placeholder = @"请输入";
+        _yiyuanTF.returnKeyType = UIReturnKeyDone;
+        _yiyuanTF.tag = 0;
+    }
+}
+- (void)setupkeshiTF:(UITableViewCell *)cell {
+    if (!_keshiTF) {
+        _keshiTF = [self getTextField:cell];
+        _keshiTF.placeholder = @"请输入";
+        _keshiTF.returnKeyType = UIReturnKeyDone;
+        _keshiTF.tag = 0;
+    }
+}
+- (void)setupzhichengTF:(UITableViewCell *)cell {
+    if (!_zhichengTF) {
+        _zhichengTF = [self getTextField:cell];
+        _zhichengTF.placeholder = @"请输入";
+        _zhichengTF.returnKeyType = UIReturnKeyDone;
+        _zhichengTF.tag = 0;
+    }
+}
+- (void)setupzhiwuTF:(UITableViewCell *)cell {
+    if (!_zhiwuTF) {
+        _zhiwuTF = [self getTextField:cell];
+        _zhiwuTF.placeholder = @"请输入";
+        _zhiwuTF.returnKeyType = UIReturnKeyDone;
+        _zhiwuTF.tag = 0;
+    }
+}
+- (void)setupdanweiTF:(UITableViewCell *)cell {
+    if (!_danweiTF) {
+        _danweiTF = [self getTextField:cell];
+        _danweiTF.placeholder = @"请输入";
+        _danweiTF.returnKeyType = UIReturnKeyDone;
+        _danweiTF.tag = 0;
+    }
+}
+- (void)setupzhiweiTF:(UITableViewCell *)cell {
+    if (!_zhiweiTF) {
+        _zhiweiTF = [self getTextField:cell];
+        _zhiweiTF.placeholder = @"请输入";
+        _zhiweiTF.returnKeyType = UIReturnKeyDone;
+        _zhiweiTF.tag = 0;
+    }
+}
+- (void)setupxuexiaoTF:(UITableViewCell *)cell {
+    if (!_xuexiaoTF) {
+        _xuexiaoTF = [self getTextField:cell];
+        _xuexiaoTF.placeholder = @"请输入";
+        _xuexiaoTF.returnKeyType = UIReturnKeyDone;
+        _xuexiaoTF.tag = 0;
+    }
+}
+- (void)setupzhuanyeTF:(UITableViewCell *)cell {
+    if (!_zhuanyeTF) {
+        _zhuanyeTF = [self getTextField:cell];
+        _zhuanyeTF.placeholder = @"请输入";
+        _zhuanyeTF.returnKeyType = UIReturnKeyDone;
+        _zhuanyeTF.tag = 0;
+    }
+}
+- (void)setupxueliTF:(UITableViewCell *)cell {
+    if (!_xueliTF) {
+        _xueliTF = [self getTextField:cell];
+        _xueliTF.placeholder = @"请输入";
+        _xueliTF.returnKeyType = UIReturnKeyDone;
+        _xueliTF.tag = 0;
+    }
+}
+- (void)setupruxuenianfenTF:(UITableViewCell *)cell {
+    if (!_ruxuenianfenTF) {
+        _ruxuenianfenTF = [self getTextField:cell];
+        _ruxuenianfenTF.placeholder = @"请输入";
+        _ruxuenianfenTF.returnKeyType = UIReturnKeyDone;
+        _ruxuenianfenTF.tag = 0;
+    }
+}
+
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField.tag == 0 || textField.tag == 4) {
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
+
+- (NSArray *)titleArr {
+    if (!_titleArr) {
+        _titleArr = @[@"* 姓名", @"* 性别", @"* 出生年月", @"   出生时刻", @"* 联系方式", @"* 地址", @"   学历", @"   其它"];
+    }
+    return _titleArr;
+}
+    
+    
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
